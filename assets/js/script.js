5964c67d158be9496546f32bf7318960efaa6f3d
Shmee <smaheilman@gmail.com>
2021-09-30 21:48:40 -0400

    question display

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@ var highScoreEl = document.querySelector(".high-scores");
 var timerEl = document.getElementById("timer");
 var startBtn = document.getElementById("startBtn");
 var questionEl = document.getElementById("question");
+var answer1El = document.getElementById("answer1");
+var answer2El = document.getElementById("answer2");
+var answer3El = document.getElementById("answer3");
+var answer4El = document.getElementById("answer4");
+
+const quizQuestions = {
+    question1:"What symbol is used to create comments in javaScript?",
+    a: "1. //",
+    b: "2. <!--",
+    c: "3.<*",
+    d: "4. /*"
+}
 
 var timeUp = function() {
  questionEl.textContent = "TIME IS UP!";
@@ -9,12 +21,10 @@ var timeUp = function() {
 }
 
 function timer() {
-    var timeLeft = 2;
+    var timeLeft = 5;
 
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 1) {
+        if (timeLeft >= 1) {
             timerEl.textContent = "Time left: " + timeLeft;
             timeLeft--;
         } 
@@ -26,7 +36,13 @@ function timer() {
     }, 1000);
 }
 
+var displayQuestion = function(){
+    questionEl.textContent = (quizQuestions.question1);
+    answer1El.textContent = (quizQuestions.a);
+    answer2El.textContent = (quizQuestions.b);
+    answer3El.textContent = (quizQuestions.c);
+    answer4El.textContent = (quizQuestions.d);
+}
 
-
-
+displayQuestion();
 startBtn.onclick = timer;
